716bd46c136347fd85467c53e727c4c71c32088f
Elia <elia.strub@lernen.siw.swiss>
2023-03-11 20:41:16 +0100

    Kategorie erstellen Tutorial umgesetzt

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import {filter, from} from 'rxjs';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
+import {Validators} from "@angular/forms";
+import {save} from "ionicons/icons";
 
 @Component({
   selector: 'app-category-modal',
@@ -29,4 +31,37 @@ export class CategoryModalComponent {
         },
       });
   }
+}
+
+// Passed into the component by the ModalController, available in the ngOnInit
+category: Category = {} as Category;
+
+readonly categoryForm: FormGroup;
+submitting = false;
+
+constructor(
+  private readonly actionSheetService: ActionSheetService,
+  private readonly categoryService: CategoryService,
+  private readonly formBuilder: FormBuilder,
+  private readonly modalCtrl: ModalController,
+  private readonly toastService: ToastService
+) {
+  this.categoryForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.maxLength(40)]],
+  });
+}
+
+save(): void {
+  this.submitting = true;
+  this.categoryService.upsertCategory(this.categoryForm.value).subscribe({
+    next: () => {
+      this.toastService.displaySuccessToast('Category saved');
+      this.modalCtrl.dismiss(null, 'refresh');
+      this.submitting = false;
+    },
+    error: (error) => {
+      this.toastService.displayErrorToast('Could not save category', error);
+      this.submitting = false;
+    },
+  });
 }
