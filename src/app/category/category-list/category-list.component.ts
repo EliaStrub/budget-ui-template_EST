e1cce8ab4a0754844de5ce7bd26c6e2022e55ca5
Elia <elia.strub@lernen.siw.swiss>
2023-03-14 21:57:55 +0100

    Kategorie anzeigen

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,17 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { from, mergeMap } from 'rxjs';
 import { CategoryModalComponent } from '../category-modal/category-modal.component';
-import { ModalController } from '@ionic/angular';
-import { Category } from '../../shared/domain';
+import {InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent} from '@ionic/angular';
+import { Category, CategoryCriteria } from '../../shared/domain';
+import { CategoryService } from '../category.service';
+import { ToastService } from '../../shared/service/toast.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
 })
 export class CategoryListComponent implements OnInit {
-  constructor(private readonly modalCtrl: ModalController) {}
+  categories: Category[] = [];
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: CategoryCriteria = { page: 0, size: 25, sort: this.initialSort };
 
-  ngOnInit() {}
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly categoryService: CategoryService,
+    private readonly toastService: ToastService
+  ) {}
+
+  private loadCategories(next: () => void = () => {}): void {
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    this.categoryService.getCategories(this.searchCriteria).subscribe({
+      next: (categories) => {
+        if (this.searchCriteria.page === 0 || !this.categories) this.categories = [];
+        this.categories.push(...categories.content);
+        this.lastPageReached = categories.last;
+        next();
+        this.loading = false;
+      },
+      error: (error) => {
+        this.toastService.displayErrorToast('Could not load categories', error);
+        this.loading = false;
+      },
+    });
+  }
+  ngOnInit(): void {
+    this.loadCategories();
+  }
 
   async openModal(category?: Category): Promise<void> {
     const modal = await this.modalCtrl.create({
@@ -20,6 +51,16 @@ export class CategoryListComponent implements OnInit {
     });
     modal.present();
     const { role } = await modal.onWillDismiss();
-    //if (role === 'refresh') this.reloadCategories();
+    if (role === 'refresh') this.reloadCategories();
   }
+  loadNextCategoryPage($event: any) {
+    this.searchCriteria.page++;
+    this.loadCategories(() => ($event as InfiniteScrollCustomEvent).target.complete());
+  }
+
+  reloadCategories($event?: any): void {
+    this.searchCriteria.page = 0;
+    this.loadCategories(() => ($event ? ($event as RefresherCustomEvent).target.complete() : {}));
+  }
+
 }
